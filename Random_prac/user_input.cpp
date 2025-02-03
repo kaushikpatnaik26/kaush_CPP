88e8502165dee9debd63db8f02fe1b4fdd7266dd
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2025-02-03 15:35:04 +0530

    Add files via upload

---
+++ Random_prac/user_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num1,num2;
+cout << "enter the val of num 1: "; // << extraction operator 
+cin >> num1; // >> Extraction Operator
+
+cout << "\nNum 2: ";
+cin >> num2;
+ 
+ cout << "\nThe sum of "<< num1 <<" and "<< num2 <<" is "<< num1+num2;
+ return 0;
+} 
