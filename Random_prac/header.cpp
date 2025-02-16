603b23b495d2018fa2a61fc9b9ed464ddf319722
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2025-02-16 20:44:21 +0530

    Add files via upload

---
+++ Random_prac/header.cpp
@@ -0,0 +1,10 @@
+#include <iostream>// pre declared header filessss
+#include "this.h" //this.h: No such file or directory.. throws error when not defined..
+// we can define out own header file and include it in the code by specifying the path..
+using namespace std;
+int main() {
+std :: cout << "shady" <<endl ;
+std:: cout << "ajbfihwef" << endl;
+// cout << "anfh" << endl;
+ return 0;
+}
